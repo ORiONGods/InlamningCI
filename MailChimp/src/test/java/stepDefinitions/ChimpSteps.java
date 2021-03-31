adb6d8ea66889e2c9bb18de0a982dffda7cc877c
ORiONGods <henrik.odfors@smaif.se>
2021-03-31 18:46:32 +0200

    Outline implemented. Steps configured.

--- MailChimp/src/test/java/stepDefinitions/ChimpSteps.java
+++ MailChimp/src/test/java/stepDefinitions/ChimpSteps.java
@@ -2,13 +2,23 @@ package stepDefinitions;
 
 import static org.junit.Assert.assertEquals;
 
+import java.time.Duration;
+
 import org.openqa.selenium.*;
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.cucumber.java.*;
 import io.cucumber.java.en.*;
 
 public class ChimpSteps {
 	private WebDriver driver;
+	String allgood = "Success | Mailchimp";
+	String uptohundred = "Enter a value less than 100 characters long";
+	String exists = "Another user with this username already exists. Maybe it's your evil twin. Spooky.";
+	String empty = "Please enter a value";
+	
+	
 	
 	@Before
 	public void openBrowser() throws InterruptedException {
@@ -18,63 +28,75 @@ public class ChimpSteps {
 		//driver.manage().window().maximize();
 	}
 
-	@Given("I have entered an email")
-	public void i_have_entered_an_email() {
-		//driver.findElement(By.id("email")).sendKeys("henrik.odfors@smaif.se");
-		driver.findElement(By.id("email")).sendKeys("forgetat");
-			
+	@Given("^I have entered an {string}$")
+	public void i_have_entered_an_email(String theEmail) {
+		if(theEmail.equals("nobody")) {  // Valid input
+			sendKeys(driver, By.id("email"), randomUser(6) + "@smaif.se");
+		} else if(theEmail.equals("")) {
+			sendKeys(driver, By.id("email"), "");
+		}
 	}
 
-	@And("I have also entered an username")
-	public void i_have_also_entered_an_username() {
-		driver.findElement(By.cssSelector("input[id=new_username]")).sendKeys("ORiONGods");
+	@And("I have also entered an {string}")
+	public void i_have_also_entered_an_username(String theUsername) {
+		if(theUsername.equals("ORiONGods")) { // Valid input
+			sendKeys(driver, By.id("new_username"), randomUser(8));
+		} else if(theUsername.equals("longUser")) {
+			sendKeys(driver, By.id("new_username"), randomUser(101));
+		} else if(theUsername.equals("trump")) {
+			sendKeys(driver, By.id("new_username"), "trump");
+		}
+		
+		//driver.findElement(By.cssSelector("input[id=new_username]")).sendKeys("ORiONGods");
 	}
 
 	@And("I have also entered a password")
 	public void i_have_also_entered_a_password() {
-		driver.findElement(By.name("password")).sendKeys("aQ!2345678");
-		
+		sendKeys(driver, By.name("password"), "aQ!23456");		
 	}
 	
 	@And("I do not want to have any spams")
 	public void i_do_not_want_to_have_any_spams() {
-		driver.findElement(By.cssSelector("input[name=marketing_newsletter]")).click();
+		click(driver, By.cssSelector("input[name=marketing_newsletter]"));
 	}
 
 	@When("I press Sign Up")
 	public void i_press_sign_up() {
 		driver.findElement(By.cssSelector("button[id=create-account]")).submit();
-		
-		String noAtSignEmail = driver.findElement(By.cssSelector(".invalid-error")).getText();
-		String noDomainEmail = driver.findElement(By.cssSelector(".invalid-error")).getText();
-		String noNameBeforeAtEmail = driver.findElement(By.cssSelector(".invalid-error")).getText();
-		String noEmail = driver.findElement(By.cssSelector("span[class=invalid-error]")).getText();
-		assertEquals("An email address must contain a single @", noAtSignEmail);
-		assertEquals("The domain portion of the email address is invalid", noDomainEmail);
-		assertEquals("The username portion of the email address is empty", noNameBeforeAtEmail);
-		assertEquals("Please enter a value", noEmail);
-		
-		String dupeUser = driver.findElement(By.cssSelector(".invalid-error")).getText();
-		String longUser = driver.findElement(By.cssSelector(".invalid-error")).getText();
-		String notValidFormatUser = driver.findElement(By.cssSelector(".invalid-error")).getText();
-		String noUser = driver.findElement(By.cssSelector("span[class=invalid-error]")).getText();
-		assertEquals("Another user with this username already exists. Maybe it's your evil twin. Spooky.", dupeUser);
-		assertEquals("Enter a value less than 100 characters long", longUser);
-		assertEquals("Your username should either be an email address or consist of letters and numbers.", notValidFormatUser);
-		assertEquals("Please enter a value", noUser);
-		
 	}
-
-	@Then("the result should be at a verification page")
-	public void the_result_should_be_at_a_verification_page() {
-		String titleCompare = driver.getTitle();
-		assertEquals("Success | Mailchimp", titleCompare);
+	
+	@Then("I will {string}")
+	public void i_will(String verify)  throws InterruptedException {
+		
+		
+		if(verify.equals(allgood)) {
+			assertEquals(verify, driver.getTitle());
+		} else if(verify.equals(uptohundred)) {
+			assertEquals(verify, driver.findElement(By.cssSelector(".invalid-error")).getText());
+		} else if(verify.equals(exists)) {
+			assertEquals(verify, driver.findElement(By.cssSelector(".invalid-error")).getText());
+		} else if(verify.equals(empty)) {
+			assertEquals(verify, driver.findElement(By.cssSelector("span[class=invalid-error]")).getText());
+		}
 	}
 	
 	@After
 	public void closeBrowser() throws InterruptedException {
 		//driver.close();
 		//driver.quit();
+	}
+	
+	private void click(WebDriver driver, By by) {
+		new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.
+				elementToBeClickable(by));
+		driver.findElement(by).click();
+	}
+	
+	private void sendKeys(WebDriver driver, By by, String keys) {		
+		new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.presenceOfElementLocated(by));
+		driver.findElement(by).sendKeys(keys);
+		//driver.findElement(by).sendKeys(Keys.ENTER);
+		//System.out.println("Sending keys");
 	}
 
 }
